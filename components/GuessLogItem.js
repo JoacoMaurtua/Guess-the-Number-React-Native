06bc6aa2d0347e52509729424b998304e2f0635a
JoacoMaurtua <joacomaurtua@gmail.com>
2022-09-20 12:06:42 -0500

    GuessLogItem component

---
+++ components/GuessLogItem.js
@@ -0,0 +1,35 @@
+import {View, Text, StyleSheet} from 'react-native';
+
+const GuessLogItem = ({roundNumber, guess}) => {
+  return (
+    <View style = {styles.listItem}>
+      <Text style={styles.textItem}>#{roundNumber}</Text>
+      <Text style={styles.textItem}>Opponent's Guess: {guess}</Text>
+    </View>
+  );
+}
+
+export default GuessLogItem;
+
+const styles = StyleSheet.create({
+  listItem:{
+    borderColor: Colors.darkGreen,
+    borderWidth: 1,
+    borderRadius: 40,
+    padding: 12,
+    marginVertical: 8,
+    backgroundColor: Colors.accentColor,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    elevation: 5,
+    shadowColor: 'black',
+    shadowOffset: {width:0, height: 0},
+    shadowOpacity: 0.25,
+    shadowRadius: 3,
+  },
+
+  textItem:{
+    fontFamily: 'open-sans',
+  }
+})
